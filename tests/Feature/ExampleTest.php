8adcc7d8e1618c1e67997a005849de1b2df6a211
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-06-04 12:49:32 +0200

    Laravel package prep

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,20 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-namespace TTBooking\WBEngine\Tests\Feature;
-
-use PHPUnit\Framework\Attributes\CoversNothing;
-use TTBooking\WBEngine\Tests\TestCase;
-
-/**
- * @internal
- */
-#[CoversNothing]
-class ExampleTest extends TestCase
-{
-    public function testExample(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+test('example', function () {
+    expect(true)->toBeTrue();
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,22 @@ declare(strict_types=1);
 
 namespace TTBooking\WBEngine\Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends OrchestraTestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [];
+    }
+
+    protected function getPackageAliases($app): array
+    {
+        return [];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        //
+    }
+}
